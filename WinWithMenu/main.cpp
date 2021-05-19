4d9b9035738297185c111317d94ad2a6cb8a4257
cyantau <cyangtau@gmail.com>
2021-05-19 20:26:48 +0800

    two types of Menu test

--- WinWithMenu/main.cpp
+++ WinWithMenu/main.cpp
@@ -1,7 +1,10 @@
-#include<Windows.h>
+#include<windows.h>
+#include"resource.h"
+#include<strsafe.h>
 
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void ErrorExit(LPTSTR lpszFunction);
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _In_ LPSTR lpCmdLine, _In_ INT nCmdShow)
 {
@@ -12,9 +15,16 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _I
 	wc.lpszClassName = L"WindowsLearn";
 	RegisterClass(&wc);
 
-	//
+	////创建菜单资源
+	//HMENU hMenuPopup = CreatePopupMenu();
+	//AppendMenu(hMenuPopup, MF_STRING, 211, L"New File");
+	//AppendMenu(hMenuPopup, MF_SEPARATOR, NULL, NULL);
+	//AppendMenu(hMenuPopup, MF_STRING, 212, L"Open File");
+	//HMENU hMenuTop = CreateMenu();
+	//AppendMenu(hMenuTop, MF_POPUP, (UINT_PTR)hMenuPopup, L"File");
+	//InsertMenu(hMenuTop, 1, MF_STRING | MF_BYPOSITION, 112, L"Edit");
 
-	HWND hWnd = CreateWindow(wc.lpszClassName, L"Windows", 
+	HWND hWnd = CreateWindow(wc.lpszClassName, L"Windows",
 		WS_OVERLAPPEDWINDOW,	//windows style
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,	//position and size
 		NULL,	//parent class
@@ -29,23 +39,113 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _I
 
 	MSG msg = {};
 	UINT msgFilterMin = 0, msgFilterMax = 0;
-	while (GetMessage(&msg,NULL,msgFilterMin,msgFilterMax))
+	while (GetMessage(&msg, NULL, msgFilterMin, msgFilterMax))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);	//线程消息分发给窗口，一个线程可有多个窗口，
 	}
+
+	//DestroyMenu(hMenuPopup);
+	//DestroyMenu(hMenuTop);
 	return 0;
 }
+
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_DESTROY:
-		{
-			PostQuitMessage(0);
-			return 0;
-		}
+	case WM_CREATE:
+	{
+		HMENU hMenu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MENU1));
+		SetMenu(hWnd, hMenu);
+		break;
 	}
-	return DefWindowProc(hWnd,uMsg,wParam,lParam);
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	case WM_COMMAND:
+	{
+		WORD menuId = LOWORD(wParam);
+		switch (menuId)
+		{
+		case ID_VIEW:
+		{
+			MessageBox(hWnd, L"你点击了View菜单", L"提示", MB_OK);
+			break;
+		}
+		case ID_NEW_FILE:
+		{
+			MessageBox(hWnd, L"你点击了NEW_FILE", L"提示", MB_OK);
+			break;
+		}
+		case ID_HELP:
+		{
+			/*		MENUITEMINFO mii = { 0 };
+					mii.cbSize = sizeof(MENUBARINFO);
+					mii.fMask = MIIM_TYPE;
+					mii.fType = MFT_STRING;
+					mii.dwTypeData = new TCHAR[64];
+					mii.cch = 64;
+					HMENU hMenu = GetMenu(hWnd);
+					BOOL ret = GetMenuItemInfo(hMenu, 4, true, &mii);
+					int numMenu = GetMenuItemCount(hMenu);
+					UINT id = GetMenuItemID(hMenu,numMenu-3);
+
+					ErrorExit((LPTSTR)L"");*/
+
+			break;
+		}
+		}
+		break;
+	}
+	case WM_RBUTTONUP:
+	{
+		HMENU hRightPopup = CreatePopupMenu();
+		AppendMenu(hRightPopup, MF_STRING, 10000, L"New");
+		AppendMenu(hRightPopup, MF_STRING, 10001, L"Open");
+		POINT pt = { LOWORD(lParam),HIWORD(lParam) };
+		ClientToScreen(hWnd, &pt);
+		TrackPopupMenu(hRightPopup, TPM_LEFTALIGN, pt.x, pt.y, 0, hWnd, NULL);//屏幕坐标系
+		DestroyMenu(hRightPopup);
+		break;
+	}
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+
+void ErrorExit(LPTSTR lpszFunction)
+{
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	// Display the error message and exit the process
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s failed with error %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
+	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	ExitProcess(dw);
 }
---
+++ WinWithMenu/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WinWithMenu.rc
+//
+#define IDR_MENU1                       101
+#define IDD_DIALOG1                     103
+#define ID_NEW_FILE                     40003
+#define ID_NEW_PROJECT                  40004
+#define ID_OPEN_FILE                    40005
+#define ID_OPEN_PROJECT                 40006
+#define ID_FILE_CLOSEPROJECT            40007
+#define ID_FILE_SAVE                    40008
+#define ID_FILE_EXIT                    40009
+#define ID_EDIT_UNDO                    40010
+#define ID_EDIT_REDO                    40011
+#define ID_EDIT_CUT                     40012
+#define ID_EDIT_PASTE                   40013
+#define ID_EDIT_COPY                    40014
+#define ID_EDIT_DELETE                  40015
+#define ID_HELP                         40017
+#define ID_TOOLS                        40018
+#define ID_VIEW                         40019
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40022
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
